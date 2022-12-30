643eabfc66a81b065a956d5a7b76019e73dcc20f
WenYuZhi <YuanWangNeu@gmail.com>
2022-12-30 12:30:28 +0800

    add log

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 LOG_FILE_PATH = './log/{}'
 
-test_ray, n_dim, n_problem = [], 50, 100
+test_ray, n_dim, n_problem = [], 50, 10
 cost_coeff = [np.random.random((n_dim, n_dim)) for i in range(n_problem)]
 
 for i in range(n_problem):
@@ -38,9 +38,9 @@ cpu_time2 = (datetime.datetime.now() - ts).seconds
 
 log_file = pd.DataFrame([results_base_line, results_ray]).T
 log_file.columns = ['without parallel', 'with parallel']
-log_file.to_csv(LOG_FILE_PATH.format('obj_vals.csv'))
+log_file.to_csv(LOG_FILE_PATH.format('obj_vals_nd_{}_np_{}.csv'.format(n_dim, n_problem)))
 
 log_file = pd.DataFrame([cpu_time1, cpu_time2]).T
 log_file.columns = ['without parallel', 'with parallel']
-log_file.to_csv(LOG_FILE_PATH.format('cpu_time.csv'))
+log_file.to_csv(LOG_FILE_PATH.format('cpu_time_nd_{}_np_{}.csv'.format(n_dim, n_problem)))
 
